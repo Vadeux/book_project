c95090217066ec379ff95f54310c83e23e58674a
Vadim Bardier <BardierVadim97@gmail.com>
2020-03-10 19:57:13 +0300

    'logout+login_fix'

--- books/urls.py
+++ books/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 	path('', views.BookListView.as_view(), name='book_list'),
 	path('register/', views.RegisterFormView.as_view(), name='register'),
 	path('login/', views.LoginFormView.as_view(), name='login'),
+	path('logout/', views.LogoutView.as_view(), name='logout'),
 	path('filter/', views.FilterBooksView.as_view(), name='filter'),
 	path('<slug:slug>/', views.BookDetailView.as_view(), name='book_detail'),
 	path('review/<int:book_id>/', views.AddReview.as_view(), name='book_review'),
